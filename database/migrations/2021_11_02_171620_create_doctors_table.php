4100d51a32673f9d30d58ba6dcaf2237cec6ae86
unknown <zackdinar1234@gmaail.com>
2021-11-02 18:32:42 +0100

    committing some database migrations

---
+++ database/migrations/2021_11_02_171620_create_doctors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDoctorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user');
+            $table->foreign('user')->references('id')->on('users');
+            $table->string('address1');
+            $table->string('address2')->nullable();
+            $table->unsignedBigInteger('speciality');
+            $table->foreign('speciality')->references('id')->on('specialities');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctors');
+    }
+}
