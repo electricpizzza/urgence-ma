23818b9f47963aa9b149c62c72e50f2f7a08a424
unknown <zackdinar1234@gmaail.com>
2021-11-11 13:29:08 +0100

    Commiting some policies for doctors and clinics

---
+++ app/Policies/DoctorPolicy.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Doctor;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class DoctorPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    // public function update(User $user, Doctor $doctor)
+    // {
+    //     return $user->id === $doctor->user->id;
+    // }
+}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Clinic;
+use App\Models\Doctor;
+use App\Policies\ClinicPolicy;
+use App\Policies\DoctorPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -14,6 +18,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        Doctor::class => DoctorPolicy::class,
+        Clinic::class => ClinicPolicy::class,
     ];
 
     /**
