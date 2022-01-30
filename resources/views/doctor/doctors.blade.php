20fe6b1212e721100ee490f8b7bd99b269d4d525
electricpizzza <zakariae.dinar@gmail.com>
2022-01-30 23:22:36 +0100

    Updatting the doctors view

--- resources/views/doctor/doctors.blade.php
+++ resources/views/doctor/doctors.blade.php
@@ -1,4 +1,4 @@
-@extends("layout.app")
+@extends("layouts.app")
 @section('content')
     <section class="page-title bg-1">
         <div class="overlay"></div>
@@ -10,10 +10,10 @@
                         <h1 class="text-capitalize mb-5 text-lg">Specalized doctors</h1>
 
                         <!-- <ul class="list-inline breadcumb-nav">
-                      <li class="list-inline-item"><a href="index.html" class="text-white">Home</a></li>
-                      <li class="list-inline-item"><span class="text-white">/</span></li>
-                      <li class="list-inline-item"><a href="#" class="text-white-50">All Doctors</a></li>
-                    </ul> -->
+                                                  <li class="list-inline-item"><a href="index.html" class="text-white">Home</a></li>
+                                                  <li class="list-inline-item"><span class="text-white">/</span></li>
+                                                  <li class="list-inline-item"><a href="#" class="text-white-50">All Doctors</a></li>
+                                                </ul> -->
                     </div>
                 </div>
             </div>
@@ -62,21 +62,25 @@
             </div>
 
             <div class="row shuffle-wrapper portfolio-gallery">
-                <div class="col-lg-3 col-sm-6 col-md-6 mb-4 shuffle-item" data-groups="[&quot;cat1&quot;,&quot;cat2&quot;]">
-                    <div class="position-relative doctor-inner-box">
-                        <div class="doctor-profile">
-                            <div class="doctor-img">
-                                <img src="images/team/1.jpg" alt="doctor-image" class="img-fluid w-100">
+                @foreach ($doctors as $doctor)
+
+                    <div class="col-lg-3 col-sm-6 col-md-6 mb-4 shuffle-item">
+                        <div class="position-relative doctor-inner-box">
+                            <div class="doctor-profile">
+                                <div class="doctor-img">
+                                    <img src="images/team/1.jpg" alt="doctor-image" class="img-fluid w-100">
+                                </div>
+                            </div>
+                            <div class="content mt-3">
+                                <h4 class="mb-0"><a href="doctor-single.html">{{ $doctor->user->name }}</a></h4>
+                                <p>{{ $doctor->theSpeciality->title }}</p>
                             </div>
                         </div>
-                        <div class="content mt-3">
-                            <h4 class="mb-0"><a href="doctor-single.html">Thomas Henry</a></h4>
-                            <p>Cardiology</p>
-                        </div>
                     </div>
-                </div>
+                @endforeach
 
-                <div class="col-lg-3 col-sm-6 col-md-6 mb-4 shuffle-item" data-groups="[&quot;cat2&quot;]">
+
+                {{-- <div class="col-lg-3 col-sm-6 col-md-6 mb-4 shuffle-item" data-groups="[&quot;cat2&quot;]">
                     <div class="position-relative doctor-inner-box">
                         <div class="doctor-profile">
                             <div class="doctor-img">
@@ -202,9 +206,9 @@
                             <p>hematology</p>
                         </div>
                     </div>
-                </div>
+                </div> --}}
 
-                <div class="col-lg-3 col-sm-6 col-md-6 mb-4 shuffle-item illustration" data-groups="[&quot;cat2&quot;]">
+                {{-- <div class="col-lg-3 col-sm-6 col-md-6 mb-4 shuffle-item illustration" data-groups="[&quot;cat2&quot;]">
                     <div class="position-relative doctor-inner-box">
                         <div class="doctor-profile">
                             <div class="doctor-img">
@@ -216,7 +220,7 @@
                             <p>Dental</p>
                         </div>
                     </div>
-                </div>
+                </div> --}}
             </div>
         </div>
     </section>
@@ -236,7 +240,5 @@
             </div>
         </div>
     </section>
-    @foreach ($doctors as $doctor)
 
-    @endforeach
 @endsection
